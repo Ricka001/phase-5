c6647bc7d981b4ff71a093907a89d8815c07560c
Anthony Ricketts <mra.ricketts@gmail.com>
2024-09-24 10:32:21 +0100

    added server and db string

--- server/server.js
+++ server/server.js
@@ -8,3 +8,18 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors());
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
+
+const dbConnectionString = process.env.DATABASE_URL;
+
+export const db = new pg.Pool({
+  connectionString: dbConnectionString,
+});
+
+app.get("/review", (req, res) => {
+  res.json({ message: "you are looking tut! tut!" });
+});
