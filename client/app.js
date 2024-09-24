545dc8622fb5c530c8c0c02488df1ab41a6a7774
Katherine Day <kmday125@gmail.com>
2024-09-24 13:07:38 +0100

    begin coding question and answer form - client js

--- client/app.js
+++ client/app.js
@@ -1,4 +1,9 @@
 const form = document.querySelector("form");
+const problemElement = document.querySelector(".problem");
+const questionForm = document.querySelector(".answer-form");
+const questionField = document.querySelector(".answer-field");
+const pointsNeeded = document.querySelector(".points-needed");
+const mistakesAllowed = document.querySelector(".mistakes-allowed");
 
 // form.addEventListener("submit", function (event) {
 //   event.preventDefault();
@@ -46,4 +51,44 @@ function handleSubmit(event) {
 
     imageTag.src = images[currentImage];
   });
+}
+
+//===========
+let state = {
+  score: 0,
+  wrongAnswers: 0,
+};
+
+function updateProblem() {
+  state.currentProblem = generateProblem();
+  problemElement.innerHTML = `${state.currentProblem.question1} ${state.currentProblem.question2} ${state.currentProblem.question3}`;
+}
+
+updateProblem();
+
+function generateProblem() {
+  return {
+    question1: "count the number of X",
+    question2: "count the number of Y",
+    question3: "count the number of Z",
+  };
+}
+
+questionForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let correctAnswer;
+  if (state.currentProblem.question1 == question1) correctAnswer = X;
+  if (state.currentProblem.question2 == question2) correctAnswer = Y;
+  if (state.currentProblem.question3 == question3) correctAnswer = Z;
+
+  if (parseInt(questionField.value, 10) === correctAnswer) {
+    state.score++;
+    pointsNeeded.textContent = 3 - state.score;
+  } else {
+    state.wrongAnswers++;
+    mistakesAllowed.textContent = 2 - state.wrongAnswers;
+  }
 }
