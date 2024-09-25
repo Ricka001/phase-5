3f708a565575e4937054a808658642510d714b47
Katherine Day <kmday125@gmail.com>
2024-09-25 16:32:52 +0100

    username, age and score sucessfully go into the db

--- client/app.js
+++ client/app.js
@@ -1,5 +1,4 @@
 const form = document.getElementById("form");
-const formData = document.getElementById("form");
 const problemElement = document.querySelector(".problem");
 const questionForm = document.getElementById("answer-form");
 const questionField = document.querySelector(".answer-field");
@@ -9,26 +8,20 @@ const imageTag = document.getElementById("image");
 const progressBar = document.querySelector(".progress-inner");
 const endMessage = document.querySelector(".end-message");
 const resetButton = document.querySelector(".reset-button");
+let username = localStorage.getItem("username");
+let age = localStorage.getItem("age");
+let score = localStorage.getItem("gameScore");
 
 console.log("Test");
 
 document.getElementById("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-
-  let username = document.getElementById("username").value;
-  let userage = document.getElementById("age").value;
-  try {
-    const res = await fetch("http://localhost:8080/get-user-data", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, userage }),
-    });
-    if (!res.ok) {
-      throw new Error("Network response failed!");
-    }
-  } catch (error) {
-    console.error("problem with fetch", error);
-  }
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData);
+  const stringifiedForm = JSON.stringify(formValues);
+  console.log(stringifiedForm);
+  localStorage.setItem("username", formValues.username);
+  localStorage.setItem("age", formValues.age);
 });
 
 let gameData = {
@@ -39,13 +32,25 @@ let gameData = {
 
 let questionArray = [
   "How many lion cubs are in this image?",
+  "How many rabbits are in this image?",
+  "How many cats are in this image?",
   "How many meerkats are in this image?",
+  "How many penguins are there in this image?",
   "How many zebras are in this image?",
+  "How many butterflies are in this image?",
 ];
 
-let answers = [6, 13, 18];
+let answers = [6, 6, 8, 13, 12, 18, 8];
 
-const images = ["images/Lions.jpg", "images/Meerkats.jpg", "images/Zebras.jpg"];
+const images = [
+  "images/Lions.jpg",
+  "images/Rabbits.jpg",
+  "images/Cats.jpg",
+  "images/Meerkats.jpg",
+  "images/Penguin.jpg",
+  "images/Zebras.jpg",
+  "images/Butterflies.jpg",
+];
 
 function updateProblem() {
   problemElement.textContent = questionArray[gameData.currentProblem];
@@ -68,7 +73,7 @@ function handleSubmit(event) {
 
   if (userAnswer === correctAnswer) {
     gameData.score++;
-    pointsNeeded.textContent = 3 - gameData.score;
+    pointsNeeded.textContent = 7 - gameData.score;
     renderProgressBar();
     // Go to the next problem
     gameData.currentProblem =
@@ -82,16 +87,10 @@ function handleSubmit(event) {
     questionField.focus();
   }
   checkLogic();
-
-  const retrievedData = localStorage.getItem("gameScore");
-  const gameScore = JSON.parse(retrievedData);
-  console.log(gameScore);
-  return gameScore;
 }
 
 function checkLogic() {
-  //if you won -- submit score to the db??
-  if (gameData.score === 3) {
+  if (gameData.score === 7) {
     endMessage.textContent = "Congrats! You won.";
     document.body.classList.add("overlay-is-open");
   }
@@ -102,7 +101,6 @@ function checkLogic() {
   //   resetGame();
   // }
 
-  //if you lost -- submit score to the db??
   if (gameData.wrongAnswers === 3) {
     endMessage.textContent = "Sorry, you lost.";
     document.body.classList.add("overlay-is-open");
@@ -111,12 +109,12 @@ function checkLogic() {
 resetButton.addEventListener("click", resetGame);
 
 function resetGame() {
-  //value from local storage gets sent to db
+  completedData();
   document.body.classList.remove("overlay-is-open");
   gameData.currentProblem = 0;
   gameData.score = 0;
   gameData.wrongAnswers = 0;
-  pointsNeeded.textContent = 3;
+  pointsNeeded.textContent = 7;
   mistakesAllowed.textContent = 2;
   updateProblem();
   renderProgressBar();
@@ -124,48 +122,25 @@ function resetGame() {
 }
 
 function renderProgressBar() {
-  progressBar.style.transform = `scaleX(${gameData.score / 6})`;
+  progressBar.style.transform = `scaleX(${gameData.score / 7})`;
 }
 
-// formData = new FormData(form);
+async function completedData() {
+  let username = localStorage.getItem("username");
+  let age = localStorage.getItem("age");
+  let score = localStorage.getItem("gameScore");
 
-// const formValues = Object.fromEntries(formData);
-
-// console.log(formValues);
-// });
-
-const username = localStorage.getItem("username");
-
-const age = localStorage.getItem("age");
-
-localStorage.setItem("username", username);
-
-localStorage.setItem("age", age);
-
-// const images = ["images/Lions.jpg", "images/Meerkats.jpg", "images/Zebras.jpg"];
-
-// const firstImage = 0;
-// const lastImage = images.length - 1;
-// let currentImage = 0;
-
-// const nextButton = document.getElementById("next");
-// nextButton.addEventListener("click", () => {
-//   const imageTag = document.getElementById("image");
-//   currentImage++;
-//   if (currentImage >= lastImage) {
-//     currentImage = 2;
-//   }
-//   imageTag.src = images[currentImage];
-// });
-
-// const prevButton = document.getElementById("prev");
-// prevButton.addEventListener("click", () => {
-//   const imageTag = document.getElementById("image");
-//   currentImage--;
-//   if (currentImage <= firstImage) {
-//     currentImage = 0;
-//     // debugger;
-//   }
-
-//   imageTag.src = images[currentImage];
-// });
+  try {
+    console.log(username);
+    const res = await fetch("http://localhost:8080/get-user-data", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, age, score }),
+    });
+    if (!res.ok) {
+      throw new Error("Network response failed!");
+    }
+  } catch (error) {
+    console.error("problem with fetch", error);
+  }
+}
