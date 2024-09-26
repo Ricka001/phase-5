a952ccccb977521dc0cb16ed759d1ed7ab8fc334
Anthony Ricketts <mra.ricketts@gmail.com>
2024-09-26 10:49:03 +0100

    changed the css

---
+++ .history/client/app_20240925150201.js
@@ -0,0 +1,145 @@
+const form = document.getElementById("form");
+const formData = document.getElementById("form");
+const problemElement = document.querySelector(".problem");
+const questionForm = document.getElementById("answer-form");
+const questionField = document.querySelector(".answer-field");
+const pointsNeeded = document.querySelector(".points-needed");
+const mistakesAllowed = document.querySelector(".mistakes-allowed");
+const imageTag = document.getElementById("image");
+const progressBar = document.querySelector(".progress-inner");
+const endMessage = document.querySelector(".end-message");
+const resetButton = document.querySelector(".reset-button");
+
+console.log("Test");
+
+document.getElementById("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let username = document.getElementById("username").value;
+  let userage = document.getElementById("age").value;
+  try {
+    const res = await fetch("http://localhost:8080/get-user-data", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, userage }),
+    });
+    if (!res.ok) {
+      throw new Error("Network response failed!");
+    }
+  } catch (error) {
+    console.error("problem with fetch", error);
+  }
+});
+
+//gameScore
+
+let gameData = {
+  score: 0,
+  wrongAnswers: 0,
+  currentProblem: 0,
+};
+
+let questionArray = [
+  "How many lion cubs are in this image?",
+  "How many meerkats are in this image?",
+  "How many zebras are in this image?",
+];
+
+let answers = [6, 13, 18];
+
+const images = ["images/Lions.jpg", "images/Meerkats.jpg", "images/Zebras.jpg"];
+
+function updateProblem() {
+  problemElement.textContent = questionArray[gameData.currentProblem];
+  imageTag.src = images[gameData.currentProblem];
+  questionField.value = "";
+  questionField.focus();
+}
+
+updateProblem();
+
+questionForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const userAnswer = parseInt(questionField.value, 10); // Convert answer to a number
+  const correctAnswer = answers[gameData.currentProblem];
+
+  const stringifiedData = JSON.stringify(gameData.score);
+
+  if (userAnswer === correctAnswer) {
+    gameData.score++;
+    pointsNeeded.textContent = 3 - gameData.score;
+    renderProgressBar();
+    // Go to the next problem
+    gameData.currentProblem =
+      (gameData.currentProblem + 1) % questionArray.length;
+    updateProblem();
+    localStorage.setItem("gameScore", stringifiedData);
+  } else {
+    gameData.wrongAnswers++;
+    mistakesAllowed.textContent = 2 - gameData.wrongAnswers;
+    questionField.value = "";
+    questionField.focus();
+  }
+  checkLogic();
+
+  const retrievedData = localStorage.getItem("gameScore");
+  const gameScore = JSON.parse(retrievedData);
+  console.log(gameScore);
+  return gameScore;
+}
+
+function checkLogic() {
+  //if you won -- submit score to the db??
+  if (gameData.score === 3) {
+    endMessage.textContent = "Congrats! You won.";
+    document.body.classList.add("overlay-is-open");
+  }
+
+  //amend the above to if the score = 3, then scroll to next level, then:
+  // if (state.score === 6) {
+  //   alert("Congrats! You won.");
+  //   resetGame();
+  // }
+
+  //if you lost -- submit score to the db??
+  if (gameData.wrongAnswers === 3) {
+    endMessage.textContent = "Sorry, you lost.";
+    document.body.classList.add("overlay-is-open");
+  }
+}
+resetButton.addEventListener("click", resetGame);
+
+function resetGame() {
+  //value from local storage gets sent to db
+  document.body.classList.remove("overlay-is-open");
+  gameData.currentProblem = 0;
+  gameData.score = 0;
+  gameData.wrongAnswers = 0;
+  pointsNeeded.textContent = 3;
+  mistakesAllowed.textContent = 2;
+  updateProblem();
+  renderProgressBar();
+  localStorage.clear();
+}
+
+function renderProgressBar() {
+  progressBar.style.transform = `scaleX(${gameData.score / 6})`;
+}
+
+// formData = new FormData(form);
+
+// const formValues = Object.fromEntries(formData);
+
+// console.log(formValues);
+// });
+
+const username = localStorage.getItem("username");
+
+const age = localStorage.getItem("age");
+
+localStorage.setItem("username", username);
+
+localStorage.setItem("age", age);
---
+++ .history/server/server_20240925134438.js
@@ -0,0 +1,78 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+//I need to set up a port for my app to listen
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
+
+//I need to set up my database pool using the connection string from the .env file
+const dbConnectionString = process.env.DATABASE_URL;
+
+export const db = new pg.Pool({
+  connectionString: dbConnectionString,
+});
+
+app.get("/phase", (req, res) => {
+  res.json({ message: "you are looking tut! tut!" });
+});
+
+//I need to set up a root route
+//You need two routes minimum
+// end point
+app.get("/phase", async (req, res) => {
+  const query = await db.query(`SELECT * FROM phase`);
+  //we can wrangle the query response to get the rows property only.
+  res.json(query.rows);
+  console.log(query);
+});
+
+//You need a route to CREATE or ADD new data to the database
+//! In your CREATE route, the request.body is an object that represents the form data coming from your client
+app.post("/phase", function (req, res) {
+  const bodyData = req.body;
+  console.log(bodyData);
+  res.json({
+    message: "Body data received",
+    location: `${bodyData.location}`,
+  });
+});
+
+// this will get users and display
+app.post("/get-user-data", async function (req, res) {
+  const { username, userage } = req.body;
+  const query = await db.query(
+    `INSERT INTO phase (user_name,age) VALUES ($1,$2) RETURNING *`,
+    [username, userage]
+  ); //when we have a parameter($1, $2, $3), we need to specify the value in square brackets after the query is finished. That value is usually provided by the user when they submit a form --> in the body data of a form (req.body.animal)
+  //   if (res.rows.length === 0) {
+  //     return res.status(404).json({ error: "User not found!" });
+  //   }
+  res.json(query.rows[0]);
+});
+// db.query(`INSERT INTO phase (user_name,game_score,age) VALUES ($1,$2,$3)`, [
+//   "phase",
+//   "21",
+//   "3",
+// ]);
+
+// display leader
+// app.get("/display-leader", async function (req, res) {
+//   const { username } = req.body;
+//   const query = await db.query(`SELECT game_score FROM phase = $3`, [
+//     leaderboard,
+//   ]); //when we have a parameter($1, $2, $3), we need to specify the value in square brackets after the query is finished. That value is usually provided by the user when they submit a form --> in the body data of a form (req.body.animal)
+//   if (res.rows.length === 0) {
+//     return res.status(404).json({ error: "No leader!" });
+//   }
+//   res.json(query.rows[0]);
+// });
